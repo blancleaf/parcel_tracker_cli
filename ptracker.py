a5ca92b60e604a1a0248b569eddd0f4a2f4549c6
mono <null_ideology@protonmail.com>
2021-03-29 15:12:55 +0300

    Add batch input support

--- ptracker.py
+++ ptracker.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 import json
+from os import path
 
 #Output coloring
 
@@ -14,34 +15,46 @@ ENDC    = '\033[0m'
 OFFSET = 18 #ASCII encoding offset used by the API
 
 if len(sys.argv) < 2:
-    print("Usage: {} <tracking_number>".format(sys.argv[0]))
+    print("Usage: {} <argument>".format(sys.argv[0]))
+    print("<argument> can be a universal tracking number or a path to a file containing 1 number per line")
     raise SystemExit
 
-msg = sys.argv[1].upper()
+numbers = []
+
+if path.exists(sys.argv[1]):
+    with open(sys.argv[1], "r") as fp:
+        numbers = fp.readlines()
+
+else:
+    numbers.append(sys.argv[1].upper())
+
 enc_msg = []
 
-for c in msg:
-    enc_msg.append(chr(ord(c) + OFFSET))
+for number in numbers:
 
-enc_msgStr = "".join(enc_msg)
+    for c in number:
+        enc_msg.append(chr(ord(c) + OFFSET))
 
-req_data = { 'trackingId': enc_msgStr, 'carrier': "Auto-Detect", 'language': "en", 'country': "Auto-Detect", 'platform': "web-desktop", 'wd': "false", 'c': "false", 'p': "2", 'l': "2", 'se': "1920x1080,Linux+x86_64,Gecko,Mozilla,Netscape,Google+Inc.,4g" }
+    enc_msgStr = "".join(enc_msg)
 
-req = requests.post("https://parcelsapp.com/api/v2/parcels", data=req_data)
+    req_data = { 'trackingId': enc_msgStr, 'carrier': "Auto-Detect", 'language': "en", 'country': "Auto-Detect", 'platform': "web-desktop", 'wd': "false", 'c': "false", 'p': "2", 'l': "2", 'se': "1920x1080,Linux+x86_64,Gecko,Mozilla,Netscape,Google+Inc.,4g" }
 
-if not req.ok:
-    print("{}Got {} HTTP code{}".format(RED, req.status_code, ENDC))
-    raise SystemExit
+    req = requests.post("https://parcelsapp.com/api/v2/parcels", data=req_data)
 
-if "error" in req.text:
-    print("{}Got {} error{}".format(RED, req.json()['error'], ENDC))
-    print("(Please check your tracking number)")
-    raise SystemExit
+    if not req.ok:
+        print("{}Got {} HTTP code{}".format(RED, req.status_code, ENDC))
+        raise SystemExit
 
-data = req.json()
+    if "error" in req.text:
+        print("{}Got {} error{}".format(RED, req.json()['error'], ENDC))
+        print("(Please check your tracking number)")
+        raise SystemExit
 
-print("{} -----------> {}".format(data['origin'], data['destination']))
-print("Status: {}\n".format(data['status']))
+    data = req.json()
 
-for state in data['states']:
-    print("{}{}\t{} carrier: {}{}\t{}".format(BLUE, state['date'], CYAN, data['carriers'][state['carrier']], ENDC, state['status']))
+    print("{}".format(number))
+    print("{} -----------> {}".format(data['origin'], data['destination']))
+    print("Status: {}\n".format(data['status']))
+
+    for state in data['states']:
+        print("{}{}\t{} carrier: {}{}\t{}".format(BLUE, state['date'], CYAN, data['carriers'][state['carrier']], ENDC, state['status']))
